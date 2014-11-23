41b0371395857a39e930e16eeca854ba505b40f7
Karol Sikora <me@karolsikora.me>
2014-11-23 23:41:41 +0100

    added factory function

--- elasticdata/__init__.py
+++ elasticdata/__init__.py
@@ -1,5 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.conf import settings
+
 from .type import Type, ValidationError
 from .manager import EntityManager, RepositoryError, EntityNotFound
+
+
+def get_entity_manager(index=None, es_settings=None):
+    if not index:
+        index = getattr(settings, 'ELASTICSEARCH_INDEX', 'default')
+    if not es_settings:
+        es_settings = getattr(settings, 'ELASTICSEARCH_CONFIG', None)
+    return EntityManager(index=index, es_settings=es_settings)
